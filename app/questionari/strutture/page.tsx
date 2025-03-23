5215fc9b894f228c1caac91386ea130d294f0de8
SiComunica <francescocro76@gmail.com>
2025-03-24 00:47:04 +0100

    fix: aggiornato riferimento a QuestionarioStruttureNew in questionari/strutture/page

--- app/questionari/strutture/page.tsx
+++ app/questionari/strutture/page.tsx
@@ -1,28 +1,12 @@
-'use client'
+"use client"
 
-import { useEffect } from 'react'
-import { useRouter } from 'next/navigation'
-import { useAuth } from '@/contexts/AuthContext'
-import QuestionarioStrutture from '@/components/questionari/QuestionarioStrutture'
+import React from 'react';
+import QuestionarioStruttureNew from '@/components/questionari/QuestionarioStruttureNew';
 
-export default function QuestionarioStrutturePage() {
-  const router = useRouter()
-  const { userType } = useAuth()
-
-  useEffect(() => {
-    // Reindirizza alla home se non autenticato
-    if (!userType) {
-      router.push('/')
-    }
-  }, [userType, router])
-
-  if (!userType) return null
-
+export default function QuestionariStrutturePage() {
   return (
-    <div className="min-h-screen bg-gray-50 py-8">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <QuestionarioStrutture />
-      </div>
+    <div className="container mx-auto p-4">
+      <QuestionarioStruttureNew />
     </div>
-  )
+  );
 } 
