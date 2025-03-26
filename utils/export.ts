e71c8b6aa3122b7aeebb4fdf891ca13dfeff3bbf
SiComunica <francescocro76@gmail.com>
2025-03-26 19:25:05 +0100

    fix: corretto importazione xlsx per export Excel

--- utils/export.ts
+++ utils/export.ts
@@ -1,7 +1,8 @@
-import * as XLSX from 'xlsx'
+import { utils, writeFile } from 'xlsx'
 import jsPDF from 'jspdf'
 import 'jspdf-autotable'
 import type { Database } from '@/types/database'
+import type { QuestionarioStruttureNew } from '@/types/questionari'
 
 // Dichiariamo l'estensione del tipo jsPDF
 declare module 'jspdf' {
@@ -15,37 +16,37 @@ type Operatore = Database['public']['Tables']['operatori']['Row']
 type Giovane = Database['public']['Tables']['giovani']['Row']
 
 type ExportData = {
-  strutture: Struttura[]
-  operatori: Operatore[]
-  giovani: Giovane[]
+  strutture: QuestionarioStruttureNew[]
+  operatori: any[]
+  giovani: any[]
 }
 
 export const exportToExcel = (data: ExportData) => {
   try {
     // Creiamo un nuovo workbook
-    const wb = XLSX.utils.book_new()
+    const wb = utils.book_new()
 
     // Se ci sono dati strutture, creiamo un foglio
     if (data.strutture.length > 0) {
-      const wsStrutture = XLSX.utils.json_to_sheet(data.strutture)
-      XLSX.utils.book_append_sheet(wb, wsStrutture, 'Strutture')
+      const wsStrutture = utils.json_to_sheet(data.strutture)
+      utils.book_append_sheet(wb, wsStrutture, 'Strutture')
     }
 
     // Se ci sono dati giovani, creiamo un foglio
     if (data.giovani.length > 0) {
-      const wsGiovani = XLSX.utils.json_to_sheet(data.giovani)
-      XLSX.utils.book_append_sheet(wb, wsGiovani, 'Giovani')
+      const wsGiovani = utils.json_to_sheet(data.giovani)
+      utils.book_append_sheet(wb, wsGiovani, 'Giovani')
     }
 
     // Se ci sono dati operatori, creiamo un foglio
     if (data.operatori.length > 0) {
-      const wsOperatori = XLSX.utils.json_to_sheet(data.operatori)
-      XLSX.utils.book_append_sheet(wb, wsOperatori, 'Operatori')
+      const wsOperatori = utils.json_to_sheet(data.operatori)
+      utils.book_append_sheet(wb, wsOperatori, 'Operatori')
     }
 
     // Salviamo il file
     const filename = `export_${new Date().toISOString().split('T')[0]}.xlsx`
-    XLSX.writeFile(wb, filename)
+    writeFile(wb, filename)
   } catch (error) {
     console.error('Errore durante l\'export Excel:', error)
     throw error
